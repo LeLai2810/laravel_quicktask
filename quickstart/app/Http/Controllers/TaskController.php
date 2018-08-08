63434684c8754de9b03d715cd8602c8541a4cb1e
Le Lai <lelai2810.it@gmail.com>
2018-08-08 16:54:22 +0700

    fix branch 1

--- quickstart/app/Http/Controllers/TaskController.php
+++ quickstart/app/Http/Controllers/TaskController.php
@@ -35,7 +35,7 @@ class TaskController extends Controller
         $task->name = $request->name;
         $task->save();
 
-        
+        return redirect('/');
     }
 
 	/**
@@ -48,6 +48,6 @@ class TaskController extends Controller
     {
         $task->delete();
 
-        return redirect('/');
+       
     }
 }
